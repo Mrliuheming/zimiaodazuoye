06322c93264c76e4bd05d0732a92f7537a671b83
Mrliuheming <2928548834@qq.com>
2024-02-19 23:50:19 +0300

    test

---
+++ test.h
@@ -0,0 +1,20 @@
+#ifndef TEST_H
+#define TEST_H
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <string>
+#include<vector>
+#include <time.h>
+using namespace cv;
+using namespace std;
+
+const int ANGLE= 1;
+const int WIDTH = 0;
+
+#define DELAT_MAX 30
+
+typedef	int filter_type;
+
+#endif 
