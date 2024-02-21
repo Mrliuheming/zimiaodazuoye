61a61cffddda15036be54699915696b891345084
Mrliuheming <154412104+Mrliuheming@users.noreply.github.com>
2024-02-21 12:11:55 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,62 +1,8 @@
 #include"test.h"
 
-<<<<<<< HEAD
 filter_type filter(filter_type effective, filter_type newe, filter_type max);
 
 RotatedRect& adjust(cv::RotatedRect& rec, const int mode);
-=======
-filter_type filter(filter_type effective_value, filter_type new_value, filter_type delat_max);
-
-filter_type filter(filter_type effective_value, filter_type new_value, filter_type delat_max)
-{
-    if ( ( new_value - effective_value > delat_max ) || ( effective_value - new_value > delat_max ))
-    {
-        new_value=effective_value;
-        return effective_value;
-    }
-    else
-    {
-        
-        return new_value;
-    }
-}
-
-RotatedRect& adjust(cv::RotatedRect& rec, const int mode)
-   {
-       using std::swap;
-
-       float& width = rec.size.width;
-       float& height = rec.size.height;
-       float& angle = rec.angle;
-
-       if (mode == WIDTH)
-       {
-           if (width < height)
-           {
-               swap(width, height);
-               angle += 90.0;
-           }
-       }
-
-       while (angle >= 90.0) angle -= 180.0;
-       while (angle < -90.0) angle += 180.0;
-
-       if (mode == ANGLE)
-       {
-           if (angle >= 45.0)
-           {
-               swap(width, height);
-               angle -= 90.0;
-           }
-           else if (angle < -45.0)
-           {
-               swap(width, height);
-               angle += 90.0;
-           }
-       }
-   return rec;
-}
->>>>>>> f4ca1bcf15cfe428fd775372f675fe2c365f2cbf
 
  
  
@@ -98,14 +44,10 @@ int main(){
     equalizeHist(hsvS[2], hsvS[2]);
     merge(hsvS, frameH);
     Mat thresHold;
-    threshold(hsvS[2], thresHold,220,255,THRESH_BINARY);
+    threshold(hsvS[2], thresHold,240,255,THRESH_BINARY);
     blur(thresHold, thresHold, Size(3,3));
     Mat element = getStructuringElement(MORPH_ELLIPSE,Size(3,3));
     dilate(thresHold, element, element);
-<<<<<<< HEAD
-=======
-
->>>>>>> f4ca1bcf15cfe428fd775372f675fe2c365f2cbf
     vector<RotatedRect> v;
     vector<RotatedRect> R;
     vector<vector<Point>> Contour;
@@ -120,11 +62,7 @@ int main(){
         RotatedRect Light_Rec = fitEllipse(Contour[i]);
         Light_Rec = adjust(Light_Rec, ANGLE);
     
-<<<<<<< HEAD
         if (Light_Rec.angle > 10)
-=======
-        if (Light_Rec.angle > 4.5)
->>>>>>> f4ca1bcf15cfe428fd775372f675fe2c365f2cbf
             continue;
         if (Light_Rec.size.width / Light_Rec.size.height > 1.8
                 ||Contour_Area / Light_Rec.size.area() < 0.68)
@@ -138,16 +76,16 @@ int main(){
         for (size_t j = i + 1; (j < v.size()); j++)
         {     RotatedRect AR;
           
-             AR.center.x = (v[i].center.x + v[j].center.x) / 2.; 
+            AR.center.x = (v[i].center.x + v[j].center.x) / 2.; 
             AR.center.y = (v[i].center.y + v[j].center.y) / 2.; 
-        float Contour_angle = abs(v[i].angle - v[j].angle); 
+            float Contour_angle = abs(v[i].angle - v[j].angle); 
             if (Contour_angle >7)
                 continue;
-                float Contour_Len1 = abs(v[i].size.height - v[j].size.height) / max(v[i].size.height, v[j].size.height);
+            float Contour_Len1 = abs(v[i].size.height - v[j].size.height) / max(v[i].size.height, v[j].size.height);
             float Contour_Len2 = abs(v[i].size.width - v[j].size.width) / max(v[i].size.width, v[j].size.width);
             if (Contour_Len1 > 0.25 || Contour_Len2 > 0.25)
                 continue;
-                 AR.angle = (v[i].angle + v[j].angle) / 2.; 
+            AR.angle = (v[i].angle + v[j].angle) / 2.; 
             
             float h, w, yD, xD;
             h = (v[i].size.height + v[j].size.height) / 2; 
@@ -162,13 +100,13 @@ int main(){
             R.push_back(AR);
             Point2f point1;
             Point2f point2;
-             point1.x=v[i].center.x;point1.y=v[i].center.y+30;
-                point2.x=v[j].center.x;point2.y=v[j].center.y-30;
-                int xm = (point1.x+point2.x)/2;
-                int ym = (point1.y+point2.y)/2;
+            point1.x=v[i].center.x;point1.y=v[i].center.y+30;
+            point2.x=v[j].center.x;point2.y=v[j].center.y-30;
+            int xm = (point1.x+point2.x)/2;
+            int ym = (point1.y+point2.y)/2;
        
-                AR.center.x = filter(AR.center.x,xm, DELAT_MAX);
-                AR.center.y = filter(AR.center.y,ym, DELAT_MAX);
+            AR.center.x = filter(AR.center.x,xm, DELAT_MAX);
+            AR.center.y = filter(AR.center.y,ym, DELAT_MAX);
          
             Mat prediction = KF.predict();
             Point predict_pt = Point((int)prediction.at<float>(0), (int)prediction.at<float>(1));
@@ -179,7 +117,7 @@ int main(){
 
             Scalar color(100, 100, 55);
                   
-                     rectangle(frame,Point(AR.center.x-30,AR.center.y-30),Point(AR.center.x+30,AR.center.y+30),color,2);
+            rectangle(frame,Point(AR.center.x-30,AR.center.y-30),Point(AR.center.x+30,AR.center.y+30),color,2);
           
 
          
